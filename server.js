e3ff7455a4810df25b3d933a05f9058d9e699a76
Michał Warchoł <imsimilar@gmail.com>
2020-05-24 22:00:21 +0200

    initial commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).catch(err=>console.log('Error: '+err));
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).catch(err=>console.log('Error message: '+err));
 
 const connection = mongoose.connection;
 connection.once('open', ()=>{
