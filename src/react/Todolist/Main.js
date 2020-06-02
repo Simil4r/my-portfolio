0d6cf0397d77c49beb8b91e3876b102afea1c681
Michał Warchoł <imsimilar@gmail.com>
2020-06-02 13:33:59 +0200

    sdgsh

--- src/react/Todolist/Main.js
+++ src/react/Todolist/Main.js
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
-import { UserContext } from './UserContext'
-import Registration from './Registration'
-import Login from './Login'
-import Home from './Home'
+import React from 'react'
+import Content from './Content';
 
-import '../../css/Todolist.css';
-
-function Main() {
-  const [user, setUser] = useState(null)
-  return (
-    <BrowserRouter>
-      <UserContext.Provider value={{user, setUser}}>
-        <Route exact path="/todolist/registration" component={Registration} />
-        <Route exact path="/todolist/login" component={Login} />
-        <Route exact path="/todolist" component={Home} />
-      </UserContext.Provider>
-    </BrowserRouter>
-  )
+const Main = () => {
+    return (
+        <div className="todolist container-fluid d-flex justify-content-center">
+            <Content />
+        </div>
+    )
 }
 
-export default Main;
+export default Main
