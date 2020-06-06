3a08628987105ce3d1888eee552d7156a1e93979
Michał Warchoł <imsimilar@gmail.com>
2020-06-07 01:12:26 +0200

    dgfg

--- src/react/Main/Portfolio.js
+++ src/react/Main/Portfolio.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import * as Scroll from "react-scroll"
 import PortfolioSite from "./PortfolioSite";
 import MWTodolist from "../../img/MW Todolist.png";
-import MWeshop from "../../img/portfolio_site_default.png";
+//import MWeshop from "../../img/portfolio_site_default.png";
 
 let Element = Scroll.Element;
 
@@ -13,7 +13,7 @@ class Portfolio extends Component {
                 <div className={"portfolio d-flex col-9 flex-column"}>
                     <h1 className={"col-12"}>Portfolio</h1>
                     <div className="d-flex col-12 flex-column flex-xl-row">
-                        <PortfolioSite title={"E-commerce Store"} img={MWeshop} link={"/e-shop"} gitLink={"#"}/>
+                        
                         <PortfolioSite title={"ToDoList"} img={MWTodolist} link={"/todolist"} gitLink={"https://github.com/Simil4r/portfolio/tree/master/src/react/Todolist"}/>
                     </div>
                 </div>
