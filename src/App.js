0c1e3d548290de68a9439caf37bc51438dc66dde
Michał Warchoł <imsimilar@gmail.com>
2020-07-22 14:09:25 +0200

    repaired

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './react/Main/Main';
 import Todolist from './react/Todolist/Main';
-import Eshop from './react/E-Shop/Main'
 import './css/App.css';
 import { LinkContext } from './LinkContext'
 import "./img/fontello-d33de368/css/fontello.css"
@@ -15,7 +14,6 @@ const App = () => {
       <LinkContext.Provider value={{link, setLink}}>
         <Route exact path='/' component={Main} />
         <Route path='/todolist' component={Todolist} />
-        <Route path="/e-shop" component={Eshop} />
       </LinkContext.Provider>
     </BrowserRouter>
   )
