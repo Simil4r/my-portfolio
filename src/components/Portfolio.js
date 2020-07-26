4c89a4a49bcca0eec843abed1bae4dca2d48f9fd
Michał Warchoł <imsimilar@gmail.com>
2020-07-26 13:27:49 +0200

    sdgfsdfg

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,22 +1,25 @@
 import React, {Component} from "react";
 import * as Scroll from "react-scroll"
 import PortfolioSite from "./PortfolioSite";
-//import MWSecrets from "../img/MW SecretsOfMountains.png";
 import MWTodolist from "../img/MW Todolist.png";
-import MWeshop from "../img/MW Fandom.png";
+import MWFandom from "../img/MW Fandom.png";
 
 let Element = Scroll.Element;
 
 class Portfolio extends Component {
+
+    componentWillUnmount(){
+        window.scrollTo({top: 0, left: 0});
+    }
+
     render(){
         return(
             <Element name="portfolio" className="portf d-flex justify-content-center align-items-start">
                 <div className={"portfolio d-flex col-9 flex-column"}>
                     <h1 className={"col-12"}>Portfolio</h1>
-                    <div className="d-flex col-12 flex-column flex-xl-row flex-wrap">
-                        
-                        <PortfolioSite title={"Breaking Bad Fandom"} img={MWeshop} external={true} link={"http://breakingbadfandom.netlify.app"} gitLink={"https://github.com/michalwarchol/breakingbadfandom"}/>
-                        <PortfolioSite title={"ToDoList"} img={MWTodolist} external={false} link={"/todolist"} gitLink={"https://github.com/michalwarchol/portfolio/tree/master/src/react/Todolist"}/>
+                    <div className="d-flex col-12 flex-column flex-xl-row">
+                        <PortfolioSite title={"Breaking Bad Fandom"} img={MWFandom} external={true} link={"http://breakingbadfandom.netlify.app"} gitLink={"https://github.com/michalwarchol/breakingbadfandom"}/>
+                        <PortfolioSite title={"ToDoList"} img={MWTodolist} external={true} link={"http://mwtodolist.netlify.app"} gitLink={"https://github.com/michalwarchol/todolist"}/>
                     </div>
                 </div>
             </Element>
