ef7d1819b425d5feeb2ebfd03f535be1c7bafb4a
Michał Warchoł <imsimilar@gmail.com>
2020-08-09 15:55:18 +0200

    added netrunners

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import * as Scroll from "react-scroll"
 import PortfolioSite from "./PortfolioSite";
+import MWNetrunners from '../img/MW Netrunners.png';
 import MWSecrets from "../img/MW SecretsOfMountains.PNG";
 import MWTodolist from "../img/MW Todolist.png";
 import MWFandom from "../img/MW Fandom.png";
@@ -15,6 +16,7 @@ class Portfolio extends Component {
                 <div className={"portfolio d-flex col-9 flex-column"}>
                     <h1 className={"col-12"}>Portfolio</h1>
                     <div className="d-flex col-12 flex-column flex-xl-row flex-wrap">
+                        <PortfolioSite title={"Net Runners"} img={MWNetrunners} external={true} link={"http://netrunners.netlify.app"} gitLink={"https://github.com/michalwarchol/netrunners"} />
                         <PortfolioSite title={"Secrets of Mountains"} img={MWSecrets} external={true} link={"http://secretsofmountains.netlify.app"} gitLink={"https://github.com/michalwarchol/secretsofmountains"} />
                         <PortfolioSite title={"Breaking Bad Fandom"} img={MWFandom} external={true} link={"http://breakingbadfandom.netlify.app"} gitLink={"https://github.com/michalwarchol/breakingbadfandom"}/>
                         <PortfolioSite title={"ToDoList"} img={MWTodolist} external={true} link={"http://mwtodolist.netlify.app"} gitLink={"https://github.com/michalwarchol/todolist"}/>
