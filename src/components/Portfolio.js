a51a91fbf7ce4062055201ecfd5db2ab18e94475
Michał Warchoł <imsimilar@gmail.com>
2021-03-15 03:24:35 +0100

    fifteen puzzle link correction

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ class Portfolio extends Component {
                 <div className={"portfolio d-flex flex-column container"}>
                     <h1>Portfolio</h1>
                     <div className="d-flex col-12 flex-column flex-xl-row flex-wrap">
-                        <PortfolioSite title="Fifteen Puzzle" img={MWFifteen}  link={"http://fifteen-puzzle.netlify.app"} gitLink={"https://github.com/michalwarchol/fifteen-puzzle"} />
+                        <PortfolioSite title="Fifteen Puzzle" img={MWFifteen}  link={"https://mw-fifteen-puzzle.netlify.app/"} gitLink={"https://github.com/michalwarchol/fifteen-puzzle"} />
                         <PortfolioSite title={"Net Runners"} img={MWNetrunners} link={"http://netrunners.netlify.app"} gitLink={"https://github.com/michalwarchol/netrunners"} />
                         <PortfolioSite title={"Secrets of Mountains"} img={MWSecrets} link={"http://secretsofmountains.netlify.app"} gitLink={"https://github.com/michalwarchol/secretsofmountains"} />
                         <PortfolioSite title={"Breaking Bad Fandom"} img={MWFandom} link={"http://breakingbadfandom.netlify.app"} gitLink={"https://github.com/michalwarchol/breakingbadfandom"}/>
